a4a1c606727eec832ed8d7bf4ae6d195568b065d
Ori Hoch <ori@uumpa.com>
2016-01-17 20:38:57 +0200

    v0.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='okscraper',
-    version='0.0.5',
+    version='0.0.6',
     description='module for scraping websites and documents',
     author='Ori Hoch',
     author_email='ori@uumpa.com',
